6da813db347cda5d53cbf1cf9d1f52295f77d8f2
Sangyeob Lee <ydeja6@gmail.com>
2022-08-31 20:59:45 +0900

    [ADD] reasumes read

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const resumesRouter = require("./resumesRouter");
+router.use("/resumes", resumesRouter.router);
+
 module.exports = router;
