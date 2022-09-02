8c8ce035f79f3893d3cc4e76095470010559c8d9
Sangyeob Lee <ydeja6@gmail.com>
2022-09-02 12:36:27 +0900

    [ADD]이력서 조회 엔드포인트

--- src/models/resumesDao.js
+++ src/models/resumesDao.js
@@ -4,11 +4,11 @@ const getResumes = async () => {
     try {
         return await AppDataSource.query(
         `SELECT
-            r.id,
-            u.name,
-            DATE_FORMAT(r.updated_at, "%Y-%c-%e") as date
-            FROM resumes r
-            INNER JOIN users u ON r.user_id = u.id`
+           r.id,
+           u.name,
+           DATE_FORMAT(r.updated_at, "%Y-%c-%e") as date
+           FROM resumes r
+           INNER JOIN users u ON r.user_id = u.id`
       );
     } catch (err) {
       const error = new Error("INVALID_DATA_INPUT");
@@ -19,8 +19,8 @@ const getResumes = async () => {
   
 const getResumesId = async (resumesId) => {
     try {
-        return await AppDataSource.query(
-        `SELECT
+        const [resume_info] = await AppDataSource.query(`
+            SELECT
             r.id,
             u.name,
             u.email,
@@ -28,14 +28,53 @@ const getResumesId = async (resumesId) => {
             r.introduction
             FROM resumes r
             INNER JOIN users u ON r.user_id = u.id
-            WHERE r.id = ${resumesId};`
-      );
+            INNER JOIN resumes_skills rs ON rs.resumes_id = r.id
+            INNER JOIN skills s ON s.id = rs.skill_id
+            WHERE r.id = ${resumesId}
+        `);
+
+        const resume_skill = await AppDataSource.query(`
+            SELECT
+            skill
+            FROM resumes r
+            INNER JOIN resumes_skills rs ON rs.resumes_id = r.id
+            INNER JOIN skills s ON s.id = rs.skill_id            
+            WHERE r.id = ${resumesId}
+        `);
+              
+        const portfolio_url = await AppDataSource.query(`
+            SELECT
+            pu.link_url
+            FROM portfolio_urls pu
+            INNER JOIN resumes r ON r.id = pu.resumes_id
+            WHERE r.id = ${resumesId}
+        `);
+
+        const user_careers = await AppDataSource.query(`
+            SELECT
+            DATE_FORMAT(uc.career_start, "%Y-%c-%e") as start_date,
+            DATE_FORMAT(uc.career_end, "%Y-%c-%e") as end_date,
+            uc.company_name,
+            uc.department
+            FROM user_careers uc
+            INNER JOIN resumes r ON r.id = uc.resumes_id            
+            WHERE r.id = ${resumesId}
+        `);
+
+        const data = {
+          resume_info,
+          resume_skill,
+          portfolio_url,
+          user_careers
+        }
+        return data;
+
     } catch (err) {
-      const error = new Error("INVALID_DATA_INPUT");
-      error.statusCode = 500;
-      throw error;
-    };
+        const error = new Error('INVALID_DATA_INPUT');
+        error.statusCode = 500;
+        throw error;
     }
+}
 
   module.exports = { 
     getResumes,
--- src/services/resumesService.js
+++ src/services/resumesService.js
@@ -1,23 +1,13 @@
 const resumesDao = require("../models/resumesDao");
 
 const getResumes = async () => {
-  try{
     const getResumes = await resumesDao.getResumes();
     return getResumes;
-   } catch (err) {
-    console.log(err);
-    return res.status(err.statusCode || 500).json({ message: err.message });
-    }
   };
 
 const getResumesId = async (resumesId) => {
-  try{
-    const getResumesId = await resumesDao.getResumesId(resumesId);
+    const getResumesId = await resumesDao.getResumesId(resumesId);   
     return getResumesId;
-    } catch (err) {
-    console.log(err);
-     return res.status(err.statusCode || 500).json({ message: err.message });
-      }
     };
   
 module.exports = {
